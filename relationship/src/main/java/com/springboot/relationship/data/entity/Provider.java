4164088b60a7c4a92ff395e057bed16f3d5b23b0
[ninano05] <[ohjy0530@duksung.ac.kr]>
2023-11-28 02:58:15 +0900

    [11.28] 9주차 스터디

---
+++ relationship/src/main/java/com/springboot/relationship/data/entity/Provider.java
@@ -0,0 +1,29 @@
+package com.springboot.relationship.data.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@Table(name = "provider")
+public class Provider extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "provider", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    @ToString.Exclude
+    private List<Product> productList = new ArrayList<>();
+}
